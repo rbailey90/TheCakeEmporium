3ba662ac7e37a0c146cbe08c2ed10fe403a8aade
staciblaschko <staci.blaschko@gmail.com>
2016-08-14 22:21:12 -0500

    Started some objects for the cart functionality

---
+++ App_Code/InvoiceItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for InvoiceItem
+/// </summary>
+public class InvoiceItem
+{
+    Cake item;
+    int quantity;
+
+	public InvoiceItem()    {}
+
+    public InvoiceItem(Cake theCake, int theQuantity)
+    {
+        this.item=theCake;
+        this.quantity=theQuantity;
+    }
+
+    public Cake Item
+    {
+        get{return item;}
+        set{item=value;}
+    }
+
+    public int Quantity
+    {
+        get{return quantity;}
+        set{quantity=value;}
+    }
+
+}
---
+++ App_Code/Order.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Order
+/// </summary>
+public class Order
+{
+    const decimal taxrate = 0.07m;
+    int orderID;
+    DateTime orderDate = DateTime.Today;
+    //List<InvoiceItem> itemList = new List<InvoiceItem>(); 
+    
+    decimal subtotal;
+    decimal tax;
+    decimal orderTotal;
+
+    public Order() 
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    /* List<InvoiceItem> ItemList
+    {
+        get { return itemList; }
+        set { itemList = value; }
+    }*/
+
+    public decimal OrderTotal
+    {
+        get { return orderTotal; }
+        set { orderTotal = value; }
+    }
+
+    public DateTime OrderDate
+    {
+        get { return orderDate; }
+        set { orderDate = value; }
+    }
+    public int OrderID
+    {
+        get { return orderID; }
+        set { orderID = value; }
+    }
+    public decimal Subtotal
+    {
+        get { return subtotal; }
+        set { subtotal = value; }
+    }
+    public decimal Tax
+    {
+        get{return tax;}
+        set{tax=value;}
+    }
+    /*
+   public void AddItem(InvoiceItem item)
+   {
+       itemList.Add(item);            
+   }
+   public void DeleteItem(InvoiceItem item)
+   {
+       itemList.Remove(item);
+   }
+   
+   public decimal CalculateTax()
+   {
+       Tax=0;
+       Subtotal=0;
+       foreach(InvoiceItem i in ItemList)
+       {
+           Subtotal+=i.Item.Price;
+       }
+       Tax = Math.Round((Subtotal * taxrate), 2);
+       return Tax;
+   }   */
+    public decimal TotalOrder()
+    { 
+        OrderTotal = 0;
+        OrderTotal=Subtotal+tax;
+        return orderTotal;
+    }
+    
+//save order method
+
+
+
+}
