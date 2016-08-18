32d78f5959805f17f81c4df4e0aba24b2e794402
staciblaschko <staci.blaschko@gmail.com>
2016-08-17 22:03:49 -0500

    Adding an add-to-cart button to Details page

--- Products/Details.aspx.cs
+++ Products/Details.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class Products_Details : System.Web.UI.Page
 {
@@ -16,4 +17,39 @@ public partial class Products_Details : System.Web.UI.Page
             lblCakeID.Text = cakeID.ToString();
         }
     }
+
+
+
+    protected void btnAddtoCart_Click(object sender, EventArgs e)
+    {
+        if(Page.IsValid)
+        {
+            Cake selectedProduct = this.GetSelectedProduct();
+            CartItemList cart = CartItemList.GetCart();
+            CartItem cartItem = cart[selectedProduct.ProductId];
+            if(cartItem==null)
+            {
+                cart.AddItem(selectedProduct, Convert.ToInt32(txtQuantity.Text));
+            }
+            else
+            {
+                cartItem.AddQuantity(Convert.ToInt32(txtQuantity.Text));
+            }
+            Response.Redirect("Cart.aspx");
+        }
+    }
+    private Cake GetSelectedProduct()
+    {
+        DataView productsTable = (DataView)
+            SqlDataSource2.Select(DataSourceSelectArguments.Empty);
+        DataRowView row = (DataRowView)productsTable[0];
+        Cake p = new Cake();
+        p.ProductId = row["ProductID"].ToString();
+        p.Name = row["Name"].ToString();
+        p.Description = row["Description"].ToString();
+        p.UnitPrice = (decimal)row["UnitPrice"];
+        p.ImageFile = row["ImageFile"].ToString();
+        return p;
+    }
+
 }
